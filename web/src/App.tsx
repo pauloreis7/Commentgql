e800b52905f5b2afc9a56698d7dadcdfbb791fa8
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-12-13 13:08:15 -0300

    add web project styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,31 @@
 import { Form } from './components/Form'
 import { Comment } from './components/Comment'
 
+import './styles.css'
+
 export function App() {
   return (
-    <>
-      <h1>Commentgql</h1>
-      
-      <Form />
+    <div id="app">
+      <main>
+        <h1>Commentgql</h1>
+        
+        <Form />
 
-      <section className="comments">
-        <Comment
-          id='1'
-          name='Paulo Reis'
-          description='Description ex'
-          handleDelete={(id) => (console.log(`${id} deleted`))}
-        />
-        <Comment
-          id='1'
-          name='Paulo Reis'
-          description='Description ex'
-          handleDelete={(id) => (console.log(`${id} deleted`))}
-        />
-      </section>
-    </>
+        <section className="comments">
+          <Comment
+            id='1'
+            name='Paulo Reis'
+            description='Description ex'
+            handleDelete={(id) => (console.log(`${id} deleted`))}
+          />
+          <Comment
+            id='1'
+            name='Paulo Reis'
+            description='Description ex'
+            handleDelete={(id) => (console.log(`${id} deleted`))}
+          />
+        </section>
+      </main>
+    </div>
   )
 }
