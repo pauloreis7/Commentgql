a8535d794d5e0049351b4e6e6ad79cae8aafc1f9
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-12-14 09:42:57 -0300

    install and config apollo and graphql to the project

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 import { App } from './App'
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:4000'
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
